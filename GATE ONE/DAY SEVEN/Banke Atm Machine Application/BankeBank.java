e23a2efe86063d2c27b67df532123d56d058e981
Wisdom Uzoma <princewizoma22@gmail.com>
2025-04-13 05:36:58 +0100

    Update BankeBank.java

--- GATE ONE/DAY SEVEN/Banke Atm Machine Application/BankeBank.java
+++ GATE ONE/DAY SEVEN/Banke Atm Machine Application/BankeBank.java
@@ -3,9 +3,9 @@ import java.util.Random;
 
 public class BankeBank {
 
-    private ArrayList<Account> accountInformations = new ArrayList<>(); // List to store all accounts
+    private ArrayList<Account> accountInformations = new ArrayList<>();
 
-    // Method to create a new account
+    
     public String createAccount(String firstName, String lastName, String pin) {
         if (!firstName.matches("[a-zA-Z\\s\\-']+")) {
             throw new IllegalArgumentException("First Name should be alphabetic");
@@ -18,18 +18,16 @@ public class BankeBank {
         }
 
         Random numberGenerator = new Random();
-        String accountNumber = String.valueOf(numberGenerator.nextInt(100000000)); // Generate random account number
+        String accountNumber = String.valueOf(numberGenerator.nextInt(100000000));
         Account userAccount = new Account(firstName, lastName, pin, accountNumber);
-        accountInformations.add(userAccount); // Add the new account to the list
+        accountInformations.add(userAccount);
         return accountNumber;
     }
 
-    // Check if the bank has accounts
     public boolean isEmpty() {
         return accountInformations.isEmpty();
     }
-
-    // Find an account by account number
+    
     public Account findAccountByAccountNumber(String accountNumber) {
         for (Account account : accountInformations) {
             if (account.getAccountNumber().equals(accountNumber)) {
@@ -39,7 +37,6 @@ public class BankeBank {
         throw new IllegalArgumentException("Account not found.");
     }
 
-    // Close an account by account number
     public void closeAccount(String accountNumber) {
         Account account = findAccountByAccountNumber(accountNumber);
         accountInformations.remove(account);
